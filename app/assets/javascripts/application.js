d0b151b4428682f4ad307d669265548badb53460
Emilie Morissette <emorissettegregoire@gmail.com>
2019-05-20 17:32:31 +0300

    Added a js dom event so that the issue cards not display when user clicks on the pull request tab

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
+
+// import {toggleTab} from '../github-tabs';
+// toggleTab();
+
+// import {active} from './github-tabs';
+// active();
